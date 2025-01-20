fcab08c9dbd352eb256b326390bab036e66b08f2
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2025-01-20 13:42:40 -0600

    Create generate_board.py

---
+++ util/generate_board.py
@@ -0,0 +1,7 @@
+# read from file
+# parse each line into a cell
+# cells stored as board
+
+# test list to see how find works in multi dim. lists
+test = [['a', 'b', 'c'], ['b', 'd']]
+print(test.find('a'))
