97a0214951afe09878ab5673f1ae59627fe1b716
QueerCodedDev <120541616+QueerCodedDev@users.noreply.github.com>
2025-03-08 20:36:22 +0000

    Started testing an additional encryption option

---
+++ value_testing/main.py
@@ -0,0 +1,7 @@
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+MAX_LENGTH = 5
+cur_length = 1
+
+for i in ALPHABET:
+    print(i)
